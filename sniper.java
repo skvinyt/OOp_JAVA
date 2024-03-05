1c7f047f781c108c83a2e416822f137a78e37f94
DenisSalt <skvinyt@yandex.ru>
2024-03-05 19:10:03 +0700

    add class sniper

---
+++ sniper.java
@@ -0,0 +1,11 @@
+public class sniper extends pers{
+    int accuarcy;
+    int disgulse;
+    
+    public sniper(int id, String name, int health, int stamina, String weapon, int accuarcy, int disgulse) {
+        super(id, name, health, stamina, weapon);
+        this.accuarcy = accuarcy;
+        this.disgulse = disgulse;
+    }
+    
+}
