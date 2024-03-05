6444c0d1200982f25218b4b494c6433d2963da7c
DenisSalt <skvinyt@yandex.ru>
2024-03-05 19:08:21 +0700

    add class magic

---
+++ magic.java
@@ -0,0 +1,11 @@
+public class magic extends pers{
+    int intelect;
+    int mana;
+    
+    public magic(int id, String name, int health, int stamina, String weapon, int intelect, int mana) {
+        super(id, name, health, stamina, weapon);
+        this.intelect = intelect;
+        this.mana = mana;
+    }
+    
+}
