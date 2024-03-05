510572849aa3293e68bd48bc54a174b01bb34b44
DenisSalt <skvinyt@yandex.ru>
2024-03-05 19:09:47 +0700

    add class robber

---
+++ robber.java
@@ -0,0 +1,7 @@
+public class robber extends pers {
+
+    public robber(int id, String name, int health, int stamina, String weapon) {
+        super(id, name, health, stamina, weapon);
+    }
+    
+}
